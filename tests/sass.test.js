1de9339c669b804bfb88c627d2de9d856be3879b
Jonny Gerig Meyer <jonny@oddbird.net>
2022-10-28 10:47:08 -0400

    true vitest example

---
+++ tests/sass.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const path = require('path');
+
+const sassTrue = require('sass-true');
+
+const sassFile = path.join(__dirname, 'test.scss');
+sassTrue.runSass({ describe, it }, sassFile);
